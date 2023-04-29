439e25f1d2d58e508f2f2718daa242cfee9acdb3
c1nc2 <127804466+c1nc2@users.noreply.github.com>
2023-04-29 11:02:03 +0900

    Create 1-brandgenerator.py

---
+++ 1-brandgenerator.py
@@ -0,0 +1,12 @@
+# greeting
+print("Welcome to the Business Name Generator!")
+
+# ask the user for the city they grow
+city = input("Which city did you grow up in?\n")
+
+# ask for their favorite color
+color = input("What is your favorite color?\n")
+
+# combination
+print("Your Business name could be " + city + " " + color)
+
